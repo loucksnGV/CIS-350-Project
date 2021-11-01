b1f21d6185a0c268a6f56ff1c4397b82b7f23b8d
loucksnGV <loucksn@mail.gvsu.edu>
2021-11-01 14:00:40 -0400

    Groundwork for GUI layout

--- Dogfood.py
+++ Dogfood.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QPixmap, QPainter
 import sys, math, urllib.request
+from PyQt6.uic.Compiler.qtproxies import QtWidgets
 
 client = MongoClient('mongodb+srv://client:default@cluster0.psr24.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = client.BookDB
@@ -14,22 +15,45 @@ class DefaultWindow(QWidget):
         
     def initDefault(self):
         self.setWindowTitle("Book Recommendation Service")
+        self.setFixedSize(1200, 800)
         
+        main_view = TitleGrid()
+        recs_view = QWidget()
+        info_view = QWidget()
+        
+        main_view.setFixedSize(865, 550)
+        recs_view.setFixedSize(865, 250)
+        info_view.setFixedSize(335, 800)
+        
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(main_view)
+        layout.addWidget(info_view)
+        layout.addWidget(recs_view)
+        
+        self.show()
+        
+        
+class TitleGrid(QScrollArea):
+    def __init__(self):
+        super().__init__()
+        
+        contents = QWidget()
         grid = QGridLayout(self)
-
         for i, v in enumerate(db.recs.find({})):
             button = RecTitle(v)
-            grid.addWidget(button, math.floor(i / 10), i % 10)
+            grid.addWidget(button, math.floor(i / 7), i % 7)
             
-        self.setLayout(grid)
-        self.show()
-
+        contents.setLayout(grid)
+        self.setWidget(contents)
+        self.setWidgetResizable(False)
 
 
 class RecTitle(QAbstractButton):
     def __init__(self, rec_data, parent=None):
         super(RecTitle, self).__init__(parent)
         self.biblio_data = db.biblio.find_one({'_id': rec_data['book_id']})
+        self.setFixedSize(113, 170) #these things aren't scaling correctly atm. look into sizepolicy?
         
         #image loading hangs the UI and really needs to be made async at some point
         url = self.biblio_data['image_url']
@@ -37,15 +61,9 @@ class RecTitle(QAbstractButton):
         self.pixmap = QPixmap()
         self.pixmap.loadFromData(img_data)
         
-        self.setMinimumSize(113, 170)
-        self.setMaximumSize(113, 170)
-
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.drawPixmap(event.rect(), self.pixmap)
-
-    #def sizeHint(self):
-    #    return self.pixmap.size()
     
     def enterEvent(self, event):
         print(self.biblio_data['title'])
